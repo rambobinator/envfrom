583bd4de72c40b0be667e18a93423fd75bb012a1
Guillaume Gelin <ramnes@numberly.com>
2020-11-17 13:29:08 +0100

    Enrich the current environment variables with Vault

--- envfrom/plugins/vault.py
+++ envfrom/plugins/vault.py
@@ -12,7 +12,7 @@ class Vault(Plugin):
     _arguments = [
         (["path"], {"help": ("Path of the secret to fetch from the Vault"
                              "servers. Multiple paths can be specified."),
-                    "nargs": "+"}),
+                    "nargs": "*"}),
         (["--addr"], {"help": ("Address of the Vault server. The default is "
                                "https://127.0.0.1:8200. This can also be "
                                "specified via the VAULT_ADDR environment "
@@ -27,13 +27,29 @@ class Vault(Plugin):
         self.engine = self.client.secrets.kv.v2
         super().__init__(self)
 
+    def get_secret(self, path):
+        key = None
+        if "#" in path:
+            path, key = path.split("#", 1)
+
+        mount_point, path = path.split("/", 1)
+        response = self.engine.read_secret_version(mount_point=mount_point,
+                                                   path=path)
+
+        data = response["data"]["data"]
+        if key:
+            return data[key]
+        return data
+
     def process(self):
         env = {}
+
+        for key, value in os.environ.items():
+            if value.startswith("vault:"):
+                env[key] = self.get_secret(value[6:])
+
         for path in self.paths:
-            mount_point, path = path.split("/", 1)
-            response = self.engine.read_secret_version(mount_point=mount_point,
-                                                       path=path)
-            for key, value in response["data"]["data"].items():
+            for key, value in self.get_secret(path):
                 env[key.upper()] = value
         return env
 
